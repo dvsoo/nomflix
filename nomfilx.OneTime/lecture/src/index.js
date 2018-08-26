c124745eb24f3b7afbc7afc02ce50c41df6bae5e
dvsoo <qpzm2480@naver.com>
2018-08-26 15:25:01 +0900

    nomfilx. Oneday Day2. lecture finish

---
+++ nomfilx.OneTime/lecture/src/index.js
@@ -0,0 +1,4 @@
+import "./something.css";
+const plus = (a = 0, b = 7) => "console.log AWESOME";
+console.log("adfdaf");
+plus(1, 1);
---
+++ nomfilx.OneTime/lecture/webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const prodConfig = require("./webpack.prod");
+const devConfig = require("./webpack.dev");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const MODE = console.log(process.env.npm_lifecycle_event);
+
+const PATHS = {
+  entry: path.resolve(__dirname, "./src/index.js")
+};
+const commonConfig = {
+  entry: ["babel-polyfill", PATHS.entry],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /{node_modules}/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html"),
+      filename: "index.html"
+    })
+  ]
+};
+
+if (MODE === "dev") {
+  const both = Object.assign({}, commonConfig, devConfig);
+  module.exports = both;
+} else if (MODE == "build") {
+  mocule.exports = Object.assign({}, commonConfig, prodConfig);
+}
